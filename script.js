c62cc3a4bfe47facbead9c7b0a0a250b8b1d87cc
Elhankrd <113446086+elhankrd@users.noreply.github.com>
2023-09-30 21:42:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+
+
+
+
+
+$("#search").on('keyup', function() {
+  var searchkrn = $("#search").val().toLowerCase();
+  $("#table2 tr").filter(function(){
+      $(this).toggle($(this).text().toLowerCase().indexOf(searchkrn) > -1)
+  });
+});
